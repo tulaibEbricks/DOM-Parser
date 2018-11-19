5d8a5dd5d2b88e90601e8272fade86481bb98d5e
Tulaib <tulaib.pirzada@ebricks-inc.com>
2018-11-19 16:54:52 +0500

    Implement get call for grabbing webpage

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 // const htmlParser = require('htmlparser2');
 // const circularJSON = require('circular-json');
-// const express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 // const request = require('request');
 // const boundingRect = require('bounding-client-rect');
-// const puppeteer = require('puppeteer');
+
 const websiteGrabber = require('./Components/webpageGrabber');
 const elementsMarker = require('./Components/elementsMarker');
 const jsonWriter = require('./Components/JSONWriter');
 
 
-// const app = express();
-const screenWidth = 1024;
-const screenMaxHeight = 2600;
-const webpageURL = 'https://www.raywenderlich.com/';
+const app = express();
+app.use(bodyParser.json());
 const screenShotName = 'webpage.png';
 const screenShotPath = screenShotName;
 const jsonFileName = 'interactiveElements.json';
 const jsonFilePath = '../' + jsonFileName;
 
-async function grabPage() {
-    const screenHeight = await websiteGrabber.setupPage(screenWidth, screenMaxHeight, webpageURL);
+app.get('/grabPage', (req, res) => {
+    const {webPage, screenWidth, screenMaxHeight} = req.query;
+    grabPage(webPage, parseInt(screenWidth), parseInt(screenMaxHeight)).then (() => {
+        res.json('Screenshot Taken!!')
+    })  
+})
+
+async function grabPage(webPage, screenWidth, screenMaxHeight) {
+    const screenHeight = await websiteGrabber.setupPage(screenWidth, screenMaxHeight, webPage);
     await websiteGrabber.takeScreenshot(screenShotPath);
     const elementsDictionary = await websiteGrabber.grabPageData();
     await elementsMarker.markInteractiveElements(elementsDictionary, screenShotPath, screenWidth, screenHeight);
@@ -27,7 +33,9 @@ async function grabPage() {
     await jsonWriter.writeJSONToFile(elementsDictionary, jsonFilePath);
 }
 
-grabPage();
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
 
 
 
