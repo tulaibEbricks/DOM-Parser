36108c6460486dee8f3ad47be9cebd77974c5a2f
Tulaib <tulaib.pirzada@ebricks-inc.com>
2018-11-19 18:22:35 +0500

    Add functionality for grabing page and marking elements in separate call

--- server.js
+++ server.js
@@ -16,11 +16,34 @@ const screenShotName = 'webpage.png';
 const screenShotPath = screenShotName;
 const jsonFileName = 'interactiveElements.json';
 const jsonFilePath = '../' + jsonFileName;
+var websiteURL;
+var pageWidth;
+var pageMaxHeight;
+var pageHeight;
+var elementsDataDictionary;
 
 app.get('/grabPage', (req, res) => {
     const {webPage, screenWidth, screenMaxHeight} = req.query;
     grabPage(webPage, parseInt(screenWidth), parseInt(screenMaxHeight)).then (() => {
         res.json('Screenshot Taken!!')
+    })  
+})
+
+app.post('/grabPageData', (req, res) => {
+    const {webPage, screenWidth, screenMaxHeight} = req.body;
+    websiteURL = webPage;
+    pageWidth = parseInt(screenWidth);
+    pageMaxHeight = parseInt(screenMaxHeight);
+    grabPageData().then (() => {
+        const responseString = 'Screenshot taken at ' + Date.now().toString()
+        res.json(responseString)
+    })  
+})
+
+app.get('/markScreenshot', (req, res) => {
+    markInteractiveElements().then (() => {
+        const responseString = 'Screenshot marked at ' + Date.now().toString()
+        res.json(responseString)
     })  
 })
 
@@ -31,6 +54,17 @@ async function grabPage(webPage, screenWidth, screenMaxHeight) {
     await elementsMarker.markInteractiveElements(elementsDictionary, screenShotPath, screenWidth, screenHeight);
     await websiteGrabber.closeBrowser();
     await jsonWriter.writeJSONToFile(elementsDictionary, jsonFilePath);
+}
+
+async function grabPageData() {
+    pageHeight = await websiteGrabber.setupPage(pageWidth, pageMaxHeight, websiteURL);
+    await websiteGrabber.takeScreenshot(screenShotPath);
+    elementsDataDictionary = await websiteGrabber.grabPageData();
+    await websiteGrabber.closeBrowser();
+}
+
+async function markInteractiveElements() {
+    await elementsMarker.markInteractiveElements(elementsDataDictionary, screenShotPath, pageWidth, pageHeight);
 }
 
 app.listen(3000, () => {
